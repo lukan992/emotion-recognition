b2e0c944e8e795d960b7e98fd985cb559a779c38
Казаков Айдар <59098717+lukan992@users.noreply.github.com>
2024-12-02 15:17:44 +0300

    Create api3.py

---
+++ api3.py
@@ -0,0 +1,43 @@
+from sys import maxsize
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse, FileResponse
+import asyncio
+import os
+import live_face2 as yolo
+
+
+app = FastAPI(debug=True)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/")
+async def read_root():
+    file_path = os.path.join(os.path.dirname(__file__), 'index.html')
+    if os.path.exists(file_path):
+        return FileResponse(file_path)
+    return HTMLResponse(content="<h1>Файл index.html не найден</h1>", status_code=404)
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    # queue = asyncio.Queue(maxsize=5)
+    try:
+        async for frame in yolo.process_frames_async():
+            await websocket.send_bytes(frame)
+    except WebSocketDisconnect:
+        print("WebSocket отключен")
+    except Exception as e:
+        print(f"WebSocket ошибка: {e}")
+    finally:
+        await websocket.close()
